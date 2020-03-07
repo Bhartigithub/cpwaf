9a618eea061ea04cb81c5d60f640a536a2538cd0
Bharti <bhartiyadav741996@gmail.com>
2020-03-08 00:05:00 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,71 @@
+<?php
+$a="about";
+include("includes/headers.php");
+			?>
+	<!-- welcome -->	
+	<div class="welcome">
+		<div class="container"> 
+			<div class="col-md-6 w3ls_welcome_right"> 
+				<div class="flexslider">
+					<ul class="slides">
+						<li>	
+							<div class="agileits_w3layouts_welcome_grid">
+								<img src="images/g1.jpg" alt=" " class="img-responsive" />
+							</div>
+						</li>
+						<li>	
+							<div class="agileits_w3layouts_welcome_grid">
+								<img src="images/g2.jpg" alt=" " class="img-responsive" />
+							</div>
+						</li>
+						<li>	
+							<div class="agileits_w3layouts_welcome_grid">
+								<img src="images/g3.jpg" alt=" " class="img-responsive" />
+							</div>
+						</li>
+					</ul>
+				</div> 
+			</div>
+			<div class="col-md-6 w3ls_welcome_left"> 
+				<div class="w3ls_welcome_right1">
+					<h3 class="agileits-title">About Us</h3>
+					<h6>Awareness of animal <span> Welfare </span>and care. </h6>
+					<p>To work as a not-for-profit organization for raising awareness of animal welfare and care.
+
+To create the most comprehensive and current directory of any individuals, organisations, charities and companies working in the fields of animal welfare and care for domestic, exotic and wild animals.
+
+To improve public awareness of animal welfare organisations and charities locally and nationally across the world.
+
+To promote animal adoption and re-homing to the general public as opposed to buying purpose bread animals. To bring animal welfare organisations closer together in order to support each other or work together where possible.</p>
+ 
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+	<!-- //welcome -->	
+	<!-- Stats -->
+	
+		<div class="container">    
+			<p>	<b><span style="color:black;">A)</span></b> To provide a cost free platform allowing animal welfare organisations, rescue & re-homing centres, animal charities, animal sponsorship & adoption organisations, lost & missing pets support networks, animal conservation & study organisations a and animal welfare and education advisors to freely promote their assistance and services to the general public.</p><br> <p><b><span style="color:black;">B)</span></b> To highlight any issues animal welfare organisations may encounter. To publicise these issues on our Urgent Appeals page and provide assistance where possible by offering free advertising and promotion to any individual, organisation or company that offers to help.To Recognising these helpers by giving them an Aid 4 Animals Angel award.</p> <br>	<p> <b><span style="color:black;">C)</span></b> To raise funds via the sale of web advertising to cover the running cost of the website and also to provide quarterly donations to the animal welfare organisations listed within the directory.</p>
+<br>
+	<p> <b> <span style="color:black;">D)</span></b> To provide a free to use Events Diary allowing promotion of fundraising events, awareness days and weeks, open days and any other event worthy of public interest regarding animal welfare and care.</p>
+<br>
+	<p><b><span style="color:black;">E)</span></b> To increase public awareness of the abuse of animals in our society, particularly in vivisection laboratories and factory farms and to educate public opinion to demand, by all lawful means, the abolition of all experiments on animals, factory farming and all other forms of animal abuse.</p>
+	<br>
+	<p><b><span style="color:black;">F)</span></b> To examine existing legislation on matters associated with the above objectives or related aspects and to promote social, legal and administrative reforms in furtherance of the above objectives.</p>
+<br>
+	<p><b><span style="color:black;">G)</span></b> To prevent exploitation of animals.
+<br>
+	<p><b><span style="color:black;">H)</span></b> To educate the public and particularly young people to a sense of moral responsibility towards animals.</p>
+<br>
+	<p><b><span style="color:black;">I)</span></b> To promote, generally, a lifestyle which does not involve the abuse of animals.
+</p> 
+		</div>
+	
+	<!-- //Stats -->   
+	
+	<?php
+			include("includes/fotter.php");
+			?>
